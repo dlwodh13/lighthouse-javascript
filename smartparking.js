525edbbec1bfab296ee54da13d7f5d12296161d5
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-19 00:09:26 -0700

    Code Challenge - smart parking

---
+++ smartparking.js
@@ -0,0 +1,65 @@
+const whereCanIPark = function (spots, vehicle) {
+  // Code here!
+
+  // j = x
+  // i = y
+  for(let i = 0; i < spots.length; i++){
+      for(let j = 0; j < spots.length; j++){
+        let place = spots[i][j];
+        switch(vehicle){
+          case 'regular' :
+            if(place === 'R'){
+              return [j,i];
+            }
+            break;
+          case 'small' :
+            if(place === "R" || place === "S"){
+              return [j,i];
+            }
+            break;
+          case 'motorcycle' :
+            if(place === "R" || place === "S" || place === "R"){
+              return [j,i];
+            }
+            break;
+        }
+      }
+  }
+  return false;
+};
+
+console.log(whereCanIPark(
+  [
+    // COLUMNS ARE X
+    // 0    1    2    3    4    5
+    ['s', 's', 's', 'S', 'R', 'M'], // 0 ROWS ARE Y
+    ['s', 'M', 's', 'S', 'r', 'M'], // 1
+    ['s', 'M', 's', 'S', 'r', 'm'], // 2
+    ['S', 'r', 's', 'm', 'r', 'M'], // 3
+    ['S', 'r', 's', 'm', 'r', 'M'], // 4
+    ['S', 'r', 'S', 'M', 'M', 'S']  // 5
+  ],
+  'regular'
+));
+
+console.log(whereCanIPark(
+  [
+    ['M', 'M', 'M', 'M'],
+    ['M', 's', 'M', 'M'],
+    ['M', 'M', 'M', 'M'],
+    ['M', 'M', 'r', 'M']
+  ],
+  'small'
+));
+
+console.log(whereCanIPark(
+  [
+    ['s', 's', 's', 's', 's', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['S', 'r', 's', 'm', 'r', 's'],
+    ['S', 'r', 's', 'm', 'R', 's'],
+    ['S', 'r', 'S', 'M', 'm', 'S']
+  ],
+  'motorcycle'
+))
