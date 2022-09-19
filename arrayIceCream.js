ca5cdc41f5a0315d0fdea8224b638b7aa48d71f1
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-18 23:48:03 -0700

    Array practice - Ice cream

---
+++ arrayIceCream.js
@@ -0,0 +1,21 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+console.log(iceCreamFlavours);
+
+// code starts below
+
+//add a flavor "root beer"
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//console.log the first flavour in the array
+console.log(iceCreamFlavours[0]);
+
+//console.log the last flavour in the array
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//total numbers of flavours
+console.log(iceCreamFlavours.length);
+
+let num = 5%"3";
+console.log(num);
