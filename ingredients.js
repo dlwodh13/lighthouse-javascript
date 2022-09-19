e87e8903a786b15451a02d29ababc4d03ddbf981
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-18 23:59:45 -0700

    Code Challenge - literating over array banana bread

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+const ingredients = ["eggs","milk","flour","sugar","baking soda","baking powder","chocolate chips","bananas"];
+
+//while loop
+console.log("ingredients for the banana bread:");
+let i = 0;
+while(i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+
+//for loop
+console.log("ingredients for the banana bread:");
+for(let i = 0; i < ingredients[i]; i++){
+  console.log(ingredients[i]);
+}
+
+//for loop backward literate
+for(let i = ingredients.length; i >= 0; i--){
+  console.log(ingredients[i]);
+}
