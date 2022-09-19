7b224032527a664852648bb1159e342b3afcd849
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-19 00:06:12 -0700

    NodeSchool JS - object

---
+++ objects.js
@@ -0,0 +1,40 @@
+/*
+# JAVASCRIPTING  
+   
+ ## OBJECTS (Exercise 15 of 20)  
+   
+  Objects are lists of values similar to arrays, except values are  
+  identified by keys instead of integers.  
+   
+  Here is an example:  
+   
+     const foodPreferences = {  
+       pizza: 'yum',  
+       salad: 'gross'  
+     }  
+   
+ ## The challenge:  
+   
+  Create a file named objects.js.  
+   
+  In that file, define a variable named pizza like this:  
+   
+     const pizza = {  
+       toppings: ['cheese', 'sauce', 'pepperoni'],  
+       crust: 'deep dish',  
+       serves: 2  
+     }  
+   
+  Use console.log() to print the pizza object to the terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+     javascripting verify objects.js  
+*/
+const pizza = {  
+  toppings: ['cheese', 'sauce', 'pepperoni'],  
+  crust: 'deep dish',  
+  serves: 2  
+}  
+
+console.log(pizza);
